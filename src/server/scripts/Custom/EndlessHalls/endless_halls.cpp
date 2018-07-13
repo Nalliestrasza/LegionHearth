153c759f334713362e0661b748a568f639f0cf3c
Hoxton22 <hache13489@gmail.com>
2018-07-13 21:18:07 +0200

    EH end, not gm mode

--- src/server/scripts/Custom/EndlessHalls/endless_halls.cpp
+++ src/server/scripts/Custom/EndlessHalls/endless_halls.cpp
@@ -109,7 +109,6 @@ public:
         {
             if (GetHitPlayer())
             {
-                GetHitPlayer()->SetGameMaster(true);
                 GetHitPlayer()->RemoveAllAuras();
 				PhasingHandler::RemovePhase(GetHitPlayer()->ToPlayer(), PHASE_GAME, true);
             }
